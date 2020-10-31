a31b81ff8ac7587d2bdb336a7f99fac81ff3cefa
Jaimerson <jaimersonsantos@ufam.edu.br>
2020-10-31 19:01:44 -0400

    :recycle: Tradução alert

--- src/main.js
+++ src/main.js
@@ -1,88 +1,92 @@
-import api from './api';
+import api from "./api";
 
 class App {
-  constructor(){
+  constructor() {
     this.repositories = [];
-    this.formEl = document.getElementById('repo-form');
-    this.listEl = document.getElementById('repo-list');
+    this.formEl = document.getElementById("repo-form");
+    this.listEl = document.getElementById("repo-list");
     this.inputEL = document.querySelector("input[name='repository']");
     this.registerHandlers();
   }
 
-  registerHandlers(){
-    this.formEl.onsubmit = event => this.addRepository(event);
+  registerHandlers() {
+    this.formEl.onsubmit = (event) => this.addRepository(event);
   }
 
-  setLoading(isLoading = true){
-    if (isLoading){
-      let loadingEl = document.createElement('span');
-      loadingEl.setAttribute('id', 'loading');
-      loadingEl.appendChild(document.createTextNode('Carregando...'));
+  setLoading(isLoading = true) {
+    if (isLoading) {
+      let loadingEl = document.createElement("span");
+      loadingEl.setAttribute("id", "loading");
+      loadingEl.appendChild(document.createTextNode("Carregando..."));
       this.formEl.appendChild(loadingEl);
     } else {
-      document.getElementById('loading').remove();
+      document.getElementById("loading").remove();
     }
   }
 
-  async addRepository(event){
+  async addRepository(event) {
     event.preventDefault();
 
     const input = this.inputEL.value;
 
-    if (input.lengh === 0){
-      alert('Campo repositório vazio!');
-      return ;
+    if (input.lengh === 0) {
+      alert("Empty repository field!");
+      return;
     }
-    
+
     this.setLoading();
 
     try {
       const response = await api.get(`/repos/${input}`);
-      const { name, description, html_url, owner : { avatar_url }} = response.data;
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url },
+      } = response.data;
       this.repositories.push({
-          name,
-          description,
-          avatar_url,
-          html_url    
+        name,
+        description,
+        avatar_url,
+        html_url,
       });
-      this.inputEL.value = '';
+      this.inputEL.value = "";
       this.render();
-    }catch (e){
-      alert('Não foi possível encontrar o repositório!');
+    } catch (e) {
+      alert("Não foi possível encontrar o repositório!");
       console.log(e);
     }
-    
+
     this.setLoading(false);
   }
 
-  render(){
-    this.listEl.innerHTML = '';
+  render() {
+    this.listEl.innerHTML = "";
 
-    this.repositories.forEach(repo => {
-      let imgEl = document.createElement('img');
-      imgEl.setAttribute('src', repo.avatar_url);
-      
-      let titleEl = document.createElement('strong');
+    this.repositories.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
       titleEl.appendChild(document.createTextNode(repo.name));
 
-      let descriptionEl = document.createElement('p');
+      let descriptionEl = document.createElement("p");
       descriptionEl.appendChild(document.createTextNode(repo.description));
 
-      let linkEl = document.createElement('a');
-      linkEl.setAttribute('href', repo.html_url);
-      linkEl.setAttribute('target', '_blank');
-      linkEl.appendChild(document.createTextNode('Acessar'));
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("href", repo.html_url);
+      linkEl.setAttribute("target", "_blank");
+      linkEl.appendChild(document.createTextNode("Acessar"));
 
-      let liEl = document.createElement('li');
+      let liEl = document.createElement("li");
       liEl.appendChild(imgEl);
       liEl.appendChild(titleEl);
       liEl.appendChild(descriptionEl);
       liEl.appendChild(linkEl);
-      
 
       this.listEl.appendChild(liEl);
     });
   }
 }
- 
+
 const MyApp = new App();
